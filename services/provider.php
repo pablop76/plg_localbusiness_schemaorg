12ba4ae736eb02cbfb2dace3fd03120b8a0e7bcf
pablop76 <pawelpoltoraczyk@gmail.com>
2026-03-07 17:44:47 +0100

    Final sync of all files. Robust provider and LocalBusiness element naming.

--- services/provider.php
+++ services/provider.php
@@ -12,26 +12,36 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
+
+// Try to resolve file path for manual loading
+$jpathFile = JPATH_PLUGINS . '/schemaorg/localbusiness/src/Extension/LocalBusiness.php';
+if (!file_exists($jpathFile)) {
+    $jpathFile = JPATH_PLUGINS . '/schemaorg/plg_localbusiness_schemaorg/src/Extension/LocalBusiness.php';
+}
+
+if (file_exists($jpathFile)) {
+    require_once $jpathFile;
+}
+
+// Ensure the class name used is exact match to what's in LocalBusiness.php
 use Joomla\Plugin\Schemaorg\LocalBusiness\Extension\LocalBusiness;
 
 return new class () implements ServiceProviderInterface {
-    /**
-     * Registers the service provider with a DI container.
-     *
-     * @param   Container  $container  The DI container.
-     *
-     * @return  void
-     */
     public function register(Container $container)
     {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                // Must pass the element name 'localbusiness'
-                $plugin = new LocalBusiness(
-                    (array) PluginHelper::getPlugin('schemaorg', 'localbusiness')
-                );
+                // Return an instance of the class
+                $element = 'localbusiness';
+                $pluginData = (array) PluginHelper::getPlugin('schemaorg', $element);
+                
+                if (empty($pluginData)) {
+                    $element = 'plg_localbusiness_schemaorg';
+                    $pluginData = (array) PluginHelper::getPlugin('schemaorg', $element);
+                }
 
+                $plugin = new LocalBusiness($pluginData);
                 $plugin->setApplication(Factory::getApplication());
 
                 return $plugin;
--- src/Extension/LocalBusiness.php
+++ src/Extension/LocalBusiness.php
@@ -67,7 +67,7 @@ final class LocalBusiness extends CMSPlugin implements SubscriberInterface
             $schemaType->addOption(Text::_('PLG_SCHEMAORG_LOCALBUSINESS_LABEL'), ['value' => 'LocalBusiness']);
         }
 
-        // Direct path for stability, as Joomla's trait might look for a different folder
+        // Direct path for stability
         $xmlPath = JPATH_PLUGINS . '/schemaorg/localbusiness/forms/schemaorg.xml';
         if (is_file($xmlPath)) {
             $form->loadFile($xmlPath);
@@ -87,13 +87,11 @@ final class LocalBusiness extends CMSPlugin implements SubscriberInterface
                 continue;
             }
 
-            // Google Validator Fix
             if (isset($entry['isPartOf'])) {
                 $entry['mainEntityOfPage'] = $entry['isPartOf'];
                 unset($entry['isPartOf']);
             }
 
-            // Absolute URLs for images
             if (!empty($entry['image'])) {
                 $entry['image'] = $this->ensureAbsoluteUrl($this->prepareImage($entry['image']));
             }
@@ -101,56 +99,9 @@ final class LocalBusiness extends CMSPlugin implements SubscriberInterface
                 $entry['logo'] = $this->ensureAbsoluteUrl($this->prepareImage($entry['logo']));
             }
 
-            // Social Media
-            if (!empty($entry['sameAs']) && is_array($entry['sameAs'])) {
-                $urls = [];
-                foreach ($entry['sameAs'] as $social) {
-                    if (!empty($social['url'])) {
-                        $urls[] = $social['url'];
-                    }
-                }
-                $entry['sameAs'] = $urls;
-            }
-
-            // Opening hours
             if (isset($entry['openingHours']) && is_string($entry['openingHours'])) {
                 $hours = explode("\n", str_replace("\r", "", $entry['openingHours']));
                 $entry['openingHours'] = array_values(array_filter(array_map('trim', $hours)));
-            }
-
-            // Coordinates
-            if (!empty($entry['geo']) && is_array($entry['geo'])) {
-                if (isset($entry['geo']['latitude'])) {
-                    $entry['geo']['latitude'] = (float) $entry['geo']['latitude'];
-                }
-                if (isset($entry['geo']['longitude'])) {
-                    $entry['geo']['longitude'] = (float) $entry['geo']['longitude'];
-                }
-            }
-
-            // Booleans
-            $boolFields = ['hasDriveThroughService', 'publicAccess', 'smokingAllowed'];
-            foreach ($boolFields as $field) {
-                if (isset($entry[$field])) {
-                    $entry[$field] = (bool) $entry[$field];
-                }
-            }
-
-            // AggregateRating
-            if (!empty($entry['aggregateRating']) && is_array($entry['aggregateRating'])) {
-                if (empty($entry['aggregateRating']['ratingValue']) && empty($entry['aggregateRating']['ratingCount'])) {
-                    unset($entry['aggregateRating']);
-                } else {
-                    if (isset($entry['aggregateRating']['ratingValue'])) {
-                        $entry['aggregateRating']['ratingValue'] = (float) $entry['aggregateRating']['ratingValue'];
-                    }
-                    if (isset($entry['aggregateRating']['bestRating'])) {
-                        $entry['aggregateRating']['bestRating'] = (float) $entry['aggregateRating']['bestRating'];
-                    }
-                    if (isset($entry['aggregateRating']['ratingCount'])) {
-                        $entry['aggregateRating']['ratingCount'] = (int) $entry['aggregateRating']['ratingCount'];
-                    }
-                }
             }
         }
 
